2f4a8cd45df7cd2b7d6f513d2d6ec888cad5f2bc
JackDyre <83248599+JackDyre@users.noreply.github.com>
2024-05-03 11:35:24 -0500

    encoding uses db

--- pad13lookup.py
+++ pad13lookup.py
@@ -17,25 +17,30 @@ conn = sqlite3.connect('pad_13_lookup.db')
 cursor = conn.cursor()
 
 
-binary = [decimal_to_binary_padded(i, 13) for i in range(8192)] + [0, 1]
-with open('8194-ids.txt', 'r') as f:
-    track_ids = eval(f.read())
+# binary = [decimal_to_binary_padded(i, 13) for i in range(8192)] + [0, 1]
+# with open('8194-ids.txt', 'r') as f:
+#     track_ids = eval(f.read())
 
-cursor.execute('''CREATE TABLE IF NOT EXISTS pad13_id_lookup (
-                    binary TEXT PRIMARY KEY,
-                    track_id TEXT
-                )''')
+# cursor.execute('''CREATE TABLE IF NOT EXISTS pad13_id_lookup (
+#                     binary TEXT PRIMARY KEY,
+#                     track_id TEXT
+#                 )''')
 
 
-for i in range(8194):
-    cursor.execute("INSERT INTO pad13_id_lookup (binary, track_id) VALUES (?, ?)", (str(binary[i]), track_ids[i]))
+# for i in range(8194):
+#     cursor.execute("INSERT INTO pad13_id_lookup (binary, track_id) VALUES (?, ?)", (str(binary[i]), track_ids[i]))
 
-conn.commit()
+# conn.commit()
 
-cursor.execute("SELECT * FROM pad13_id_lookup")
-rows = cursor.fetchall()
-for row in rows:
-    print(row)
+# cursor.execute("SELECT * FROM pad13_id_lookup")
+# rows = cursor.fetchall()
+# for row in rows:
+#     print(row)
+
+
+cursor.execute("SELECT * FROM pad13_id_lookup WHERE binary = ?", (str(decimal_to_binary_padded(6000,13)),))
+track_id = cursor.fetchall()
+print(track_id[0])
 
 cursor.close()
 conn.close()
