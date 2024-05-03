f3ac3f845395f455bfd861a69b4f821c35b5c23e
JackDyre <83248599+JackDyre@users.noreply.github.com>
2024-05-02 23:55:08 -0500

    created pad13 db

---
+++ pad13lookup.py
@@ -0,0 +1,41 @@
+import sqlite3
+
+
+def decimal_to_binary_padded(decimal, pad):
+    binary = ""
+    while decimal > 0:
+        remainder = decimal % 2
+        binary = str(remainder) + binary
+        decimal = decimal // 2
+    binary = binary.zfill(pad)
+    return [int(bit) for bit in binary]
+
+
+
+conn = sqlite3.connect('pad_13_lookup.db')
+
+cursor = conn.cursor()
+
+
+binary = [decimal_to_binary_padded(i, 13) for i in range(8192)] + [0, 1]
+with open('8194-ids.txt', 'r') as f:
+    track_ids = eval(f.read())
+
+cursor.execute('''CREATE TABLE IF NOT EXISTS pad13_id_lookup (
+                    binary TEXT PRIMARY KEY,
+                    track_id TEXT
+                )''')
+
+
+for i in range(8194):
+    cursor.execute("INSERT INTO pad13_id_lookup (binary, track_id) VALUES (?, ?)", (str(binary[i]), track_ids[i]))
+
+conn.commit()
+
+cursor.execute("SELECT * FROM pad13_id_lookup")
+rows = cursor.fetchall()
+for row in rows:
+    print(row)
+
+cursor.close()
+conn.close()
