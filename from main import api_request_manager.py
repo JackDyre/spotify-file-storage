7a67860841fa4ffc8c0a5ae8def8a675b77a49b3
JackDyre <officialjackdire@gmail.com>
2024-05-05 18:33:59 -0500

    !!!!! DUPLICATES IN ID STRINGS!!!! api banned

---
+++ from main import api_request_manager.py
@@ -0,0 +1,3 @@
+from main import api_request_manager
+
+x = api_request_manager.send_request(request=api_request_manager.sp.track, track_id='https://open.spotify.com/track/27IihsrNynCBkqqw7UoZq7?si=058c7b91f3064e5a')
