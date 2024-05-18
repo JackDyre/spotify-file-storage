38216820f6026f628c0df87740290bd1de68a7e2
JackDyre <83248599+JackDyre@users.noreply.github.com>
2024-05-18 06:08:41 +0100

    id-getter.py is functional again

--- fetch-track-ids/id-getter.py
+++ fetch-track-ids/id-getter.py
@@ -1,11 +1,5 @@
-import sys
+from main import *
 
-sys.path.append('../spotify-file-storage')
-
-from main import api_request_manager
-from main import print_progress_bar
-
-sys.path.append('/fetch-track-ids')
 
 with open('new-id-list.txt', 'r') as f:
     track_ids = eval(f.read())
@@ -16,9 +10,8 @@ with open('artist-queue.txt', 'r') as f:
 with open('seen-artists.txt', 'r') as f:
     seen_artists = eval(f.read())
 
-
 try:
-    while (artist_queue) and (len(track_ids) < ((2 ** 16) + 2)):
+    while artist_queue and (len(track_ids) < ((2 ** 17) + 2)):
         artist_id = artist_queue[0]
 
         artist = api_request_manager.send_request(request=api_request_manager.sp.artist, artist_id=artist_id)
@@ -27,7 +20,8 @@ try:
         albums: list = []
         offset = 0
         while True:
-            album_batch = api_request_manager.send_request(request=api_request_manager.sp.artist_albums, artist_id=artist_id, limit=50, offset=offset)
+            album_batch = api_request_manager.send_request(request=api_request_manager.sp.artist_albums,
+                                                           artist_id=artist_id, limit=50, offset=offset)
             albums.extend(album_batch['items'])
             offset += 50
             if not album_batch['next']:
@@ -44,24 +38,23 @@ try:
                     if not ((artist['id'] in artist_queue) or (artist['id'] in seen_artists)):
                         artist_queue.append(artist['id'])
         print('\n\n')
-        print(f'Track List Length: {len(track_ids)}/{2**16 + 2} | {100 * len(track_ids)/(2**16 + 2)}')
+        print(f'Track List Length: {len(track_ids)}/{2 ** 16 + 2} | {100 * len(track_ids) / (2 ** 16 + 2)}')
         print(f'Artist Queue Length: {len(artist_queue)}')
         print('\n\n\n')
 
         seen_artists.append(artist_queue.pop(0))
 
         with open('new-id-list.txt', 'w') as f:
-            f.write(str(track_ids)) 
+            f.write(str(track_ids))
         with open('artist-queue.txt', 'w') as f:
-            f.write(str(artist_queue))    
+            f.write(str(artist_queue))
         with open('seen-artists.txt', 'w') as f:
-            f.write(str(seen_artists)) 
+            f.write(str(seen_artists))
 
 finally:
     with open('new-id-list.txt', 'w') as f:
-        f.write(str(track_ids)) 
+        f.write(str(track_ids))
     with open('artist-queue.txt', 'w') as f:
-        f.write(str(artist_queue))    
+        f.write(str(artist_queue))
     with open('seen-artists.txt', 'w') as f:
-        f.write(str(seen_artists)) 
-    
+        f.write(str(seen_artists))
