4f7f18a5c1a5571c9100450f4159ce6b8ff23800
JackDyre <83248599+JackDyre@users.noreply.github.com>
2024-05-20 02:38:08 +0100

    ...

--- recallibrate.py
+++ recallibrate.py
@@ -1,5 +1,5 @@
 import sqlite3
-from main import api_request_manager
+from main import api_request_manager, batch
 
 with sqlite3.connect("13bit_ids.db") as conn:
     cursor = conn.cursor()
@@ -13,6 +13,13 @@ for i in range(len(track_ids)):
     assert track_identifiers[i][1] == track_ids[i][0]
     original_track_tuples.append((track_ids[i][0], track_ids[i][1], track_identifiers[i][0]))
 
-print(original_track_tuples)
+for chunk in batch(original_track_tuples,10_000):
+    playlist_id = api_request_manager.send_request(
+        request=api_request_manager.sp.user_playlist_create,
+        user=api_request_manager.user_id,
+        name='test'
+    )['id']
+
+    api_request_manager.add_tracks_to_playlist()
 
 
