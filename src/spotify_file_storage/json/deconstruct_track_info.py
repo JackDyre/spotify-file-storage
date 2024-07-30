2da03e0c5fdd72e1e9eef5af44b142ec288daccc
Jack Dyre <83248599+JackDyre@users.noreply.github.com>
2024-07-29 19:41:43 -0500

    deconstructed trackinfo.json

---
+++ src/spotify_file_storage/json/deconstruct_track_info.py
@@ -0,0 +1,15 @@
+from itertools import batched
+from pathlib import Path
+import os
+
+with Path("src/spotify_file_storage/json/track_info.json").open("r") as f:
+    track_info = f.read()
+
+batched_track_info = batched(track_info, 45_000_000)
+
+for i, batch in enumerate(batched_track_info):
+    with Path(f"src/spotify_file_storage/json/track_info/track_info{i}.json").open("w") as f:
+        f.write("".join(batch))
+
+
+os.remove("src/spotify_file_storage/json/track_info.json")
