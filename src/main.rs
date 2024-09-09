8e55201ecd77d0aab2d7941bee3d60292bd8909b
Jack Dyre <83248599+JackDyre@users.noreply.github.com>
2024-09-08 22:18:30 -0500

    .

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use sfs::Credentials;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let creds = Creds::new("", "");
+    let creds = Credentials::new("", "");
     let creds = creds.get_auth_code()?;
 
     let token = creds.get_access_token().await?;
