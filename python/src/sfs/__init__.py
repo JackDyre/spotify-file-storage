787d9cdbc71f19a8b83477dd10d677841edfe915
Jack Dyre <83248599+JackDyre@users.noreply.github.com>
2024-09-20 15:38:05 -0500

    removed old python files

--- python/src/sfs/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Spotify File Storage."""
-
-from ._cli import _parse_arguments
-
-
-def main() -> None:
-    """Run main logic for module."""
-    _parse_arguments()
--- python/src/sfs/__main__.py
+++
@@ -1,5 +0,0 @@
-"""Module entrypoint."""
-
-from . import main
-
-main()
